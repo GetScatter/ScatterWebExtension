3be38448e8f3b66fc352da967f9614ec532b614b
Shai James <skyandbeyond@gmail.com>
2018-06-17 10:08:52 +0300

    onboaording

--- src/migrations/versions/5.0.4.js
+++ src/migrations/versions/5.0.4.js
@@ -12,6 +12,7 @@ export const m5_0_4 = async scatter => {
         if(endorsedNetwork) {
             const endorsedNetwork = endorsedNetworks.find(endorsed => endorsed.host === network.host);
             network.name = endorsedNetwork.name;
+            network.port = endorsedNetwork.port;
             network.protocol = endorsedNetwork.protocol;
         } else {
             network.protocol = network.port === 443 ? 'https' : 'http'
--- src/migrations/versions/version.js
+++ src/migrations/versions/version.js
@@ -1,3 +1,4 @@
 export * from './4.0.1';
 export * from './5.0.2';
 export * from './5.0.4';
+export * from './6.0.0';
